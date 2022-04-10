5c4aa8913511634a964ab6bd5a3ad6469b443135
norbertSviatko <informatikasviatko@gmail.com>
2022-04-10 16:32:44 +0200

    updated

--- server/app.js
+++ server/app.js
@@ -11,9 +11,9 @@ const __dirname = path.dirname(__filename);
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-
+/*
 app.use('/', router);
-
+*/
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
