fd56f6b290fd2b3b1e4ae461fe550efff0471356
norbertSviatko <informatikasviatko@gmail.com>
2022-04-10 17:16:17 +0200

    updated

--- server/app.js
+++ server/app.js
@@ -8,12 +8,12 @@ import router from './routes/posts.js';
 import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
+
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
-/*
+
 app.use('/', router);
-*/
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
