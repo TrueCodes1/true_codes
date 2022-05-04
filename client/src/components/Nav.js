7a5e0a4529a3bcd6562b8403108b7f60798b2f39
norbertSviatko <informatikasviatko@gmail.com>
2022-05-04 22:04:10 +0200

    updated

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -907,7 +907,6 @@ function Nav(props) {
         }
 
         const showMobileMenu = () => {
-            alert('now')
             /*
             $('#burger-mobile').addClass('fall');
             $('#burger-mobile').on('animationend', () => {
